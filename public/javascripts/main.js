3d48def8edb162de6e59a34d28bfcadb5c03648a
Howard Bates <hbates@northmen.org>
2016-07-07 15:27:27 -0400

    asdf

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,14 +11,46 @@ const FADE = require('./FadeStuff');
 
 class main {
      constructor() {
+          this.initials = [];
           FADE.doFade('in', 'title');
-          FADE.doFade('out', 'mainInfo');
+          document.getElementById('mainInfo').style.visibility = 'hidden';
+          document.getElementById('replacing').style.visibility = 'hidden';
+          document.getElementById('certified').style.visibility = 'hidden';
+          document.getElementById('certifyButton').style.visibility = 'hidden';
           main.handleNew();
+          main.handleReplacement();
+          this.handleInitials();
      }
 
      static handleNew() {
           document.getElementById('startNew').addEventListener('click', () => {
                FADE.doFade('in', 'mainInfo');
+               FADE.doFade('out', 'buttons');
+          });
+     }
+
+     static handleReplacement() {
+          document.getElementById('replacement').addEventListener('click', () => {
+               if (document.getElementById('replacement').checked) {
+                    FADE.doFade('in', 'replacing');
+               } else {
+                    FADE.doFade('out', 'replacing');
+               }
+          });
+     }
+
+     handleInitials() {
+          document.getElementById('initialsLL').addEventListener('change', () => {
+               let initials = document.getElementById('initialsLL').value.toUpperCase();
+               if (initials === '') {
+                    this.initials[0] = null;
+               } else if (initials !== 'LL') {
+                    alert('Incorrect value. Please try again.');
+                    document.getElementById('initialsLL').value = null;
+               } else {
+                    this.initials[0] = initials;
+               }
+               console.log(this.initials[0]);
           });
      }
 }
